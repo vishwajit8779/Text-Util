77dc61dce4d8dcfec94f12b7791cee30ae4df394
vishwajit8779 <44158497+vishwajit8779@users.noreply.github.com>
2020-06-23 00:24:13 +0530

    Update urls.py

--- textutil/urls.py
+++ textutil/urls.py
@@ -17,19 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', views.index, name='index'),
-#     path('about', views.about, name='about')
-# ]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('analyzed', views.analyzed, name='analyzed'),
-    # path('removepunc', views.removepunc, name='rempun'),
-    # path('capitalizefirst', views.capfirst, name='capfirst'),
-    # path('newlineremove', views.newlineremove, name='newlineremove'),
-    # path('spaceremove', views.spaceremove, name='spaceremove'),
-    # path('charcount', views.charcount, name='charcount'),
+    path('analyze', views.analyze, name='analyze'),
+   
 
 ]
